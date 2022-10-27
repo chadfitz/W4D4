6dce32e7b01e4b5db59e562e42b036c065f8b64f
chad fitzgerald <fitzchad@gmail.com>
2022-10-27 16:59:40 -0700

    Partial complete hand

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -1,8 +1,12 @@
 class Hand
-
+    attr_accessor :points
 
     def initialize
-
+        deck = Deck.new
+        # @player_1 = Player.new 
+        # @player_2 = Player.new
+        
+        @points = 0
     end
 
 
