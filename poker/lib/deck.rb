0f053a7a0e5119001ffffea4d71ad80cc52a98db
chad fitzgerald <fitzchad@gmail.com>
2022-10-27 16:36:34 -0700

    Work on deck

--- poker/lib/deck.rb
+++ poker/lib/deck.rb
@@ -1,6 +1,23 @@
+require_relative "card"
+
 class Deck
+    attr_reader :discard, :cards
 
     def self.generate_deck
-        
+        arr = [2,3,4,5,6,7,8,9,10,:J,:Q,:K,:A]
+        suits_arr = [:D, :S, :C, :H]
+        deck = []
+
+        arr.each do |value|
+            suits_arr.each do |suit|
+                deck << Card.new(value, suit)
+            end
+        end
+        deck
+    end
+
+    def initialize
+        @cards = Deck.generate_deck
+        @discard = []
     end
 end
--- poker/spec/deck_spec.rb
+++ poker/spec/deck_spec.rb
@@ -10,8 +10,9 @@ require 'deck'
 
 
 describe Deck do
-    sub
-    descibe "::generate_deck" do
+    subject(:deck) {Deck.new}
+
+    describe "::generate_deck" do
         it "has array of length 52" do
             expect(Deck.generate_deck.length).to eq(52)
         end
@@ -21,6 +22,12 @@ describe Deck do
 
     end
     describe "#initialize" do
+        it "creates a deck using ::generate_deck" do
+            expect(Deck.cards).to eq(Deck.generate_deck)
+        end
 
+        it "creates an empty discard pile" do
+            expect(deck.discard).to eq([])
+        end
     end
 end
